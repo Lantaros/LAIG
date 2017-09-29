48d1dc6e099cf17e347296e285d04cebf5d46f3b
up201503447 <up201503447@fe.up.pt>
2017-09-29 16:17:50 +0100

    updated MySceneGraph, myGraphLeaf and added primitives

--- MyGraphLeaf.js
+++ MyGraphLeaf.js
@@ -3,6 +3,20 @@
  * @constructor
 **/
 
-function MyGraphLeaf(graph, xmlelem) {
+function MyGraphLeaf(graph, leafInfo) {
+
+	this.graph = graph;
+
+	if (leafInfo.attributes.length == 3){ //id, type and args
+		this.id = leafInfo.attributes.item(0).value;		
+		this.type = leafInfo.attributes.item(1).value;	
+		this.args = leafInfo.attributes.item(2).value;	
+	}
+	else {
+		this.type = leafInfo.attributes.item(0).value;	
+		this.args = leafInfo.attributes.item(1).value;	
+	}
+
+
 }
 
