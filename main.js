6f4e8ae07566edf1995bc676ee2de127d64e0cca
up201503447 <up201503447@fe.up.pt>
2017-10-09 18:38:36 +0100

    upd

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor)
 
-	//var filename=getUrlVars()['file'] || "demo.xml";
     //var filename=getUrlVars()['file'] || "demoTexLuz.xml";
-    var filename=getUrlVars()['file'] || "demo.xml";
+   // var filename=getUrlVars()['file'] || "demo.xml";
+    var filename=getUrlVars()['file'] || "testeSaloon.xml";
 
 
 	// create and load graph, and associate it to scene.
