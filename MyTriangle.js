e9d4f933790e170f0d751980319894e5d11d86b3
Lantaros <ruileixo@hotmail.com>
2017-10-18 23:51:01 +0100

    Fixed MyTriangle's normals, by using a cross product

--- MyTriangle.js
+++ MyTriangle.js
@@ -27,18 +27,40 @@ MyTriangle.prototype.constructor=MyTriangle;
     this.baseTexCoords = new Array();
 
 
+
     this.vertices.push(this.args[0], this.args[1], this.args[2]); //P0
     this.vertices.push(this.args[3], this.args[4], this.args[5]); //P1
     this.vertices.push(this.args[6], this.args[7], this.args[8]); //P2
 
-    this.normals.push(0, 1, 0);
-    this.normals.push(0, 1, 0);
-    this.normals.push(0, 1, 0);
+
+    //CROSS PRODUCT AxB = -BxA
+    var A = [this.args[0], this.args[1], this.args[2]];
+    var B = [this.args[3], this.args[4], this.args[5]];
+    var C = [this.args[6], this.args[7], this.args[8]];
+
+    var vecAC = [C[0] - A[0], C[1] - A[1], C[2] - A[2]];
+    var vecAB = [B[0] - A[0], B[1] - A[1], B[2] - A[2]];
+
+    // //Neg AB
+    // var vecAB = [-B[0] + A[0], -B[1] + A[1], -B[3] + A[2]];
+
+    //x = 0, y = 1, z= 2
+    var normalVec = [
+      vecAB[1]*vecAC[2]-vecAC[1]*vecAB[2],
+      -1*(vecAB[0]*vecAC[2]-vecAC[0]*vecAB[2]),
+      vecAB[0]*vecAC[1]-vecAC[0]*vecAB[1],
+    ];
+
+    for(let i = 0; i < 3; i++)
+      this.normals.push(normalVec[0], normalVec[1], normalVec[2]);
+    // this.normals.push(0, 1, 0);
+    // this.normals.push(0, 1, 0);
+    // this.normals.push(0, 1, 0);
 
 
     //a -> dist(P0, P2)
     var a = Math.sqrt( (this.args[0]-this.args[6]) * (this.args[0]-this.args[6]) +
-                       (this.args[1]-this.args[7]) * (this.args[1]-this.args[7]) + 
+                       (this.args[1]-this.args[7]) * (this.args[1]-this.args[7]) +
                        (this.args[2]-this.args[8]) * (this.args[2]-this.args[8]));
 
 
